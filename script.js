33c7f1993cb233598a79c68803b9263d11d88bcb
Maxime PAQUET <maxime.paquet1245@aol.fr>
2021-07-18 18:24:03 +0200

    Add Button function

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
 
 function choseAFaire(){
+    let inputTextContent = document.getElementById("btn-text").value;
+    console.log(inputTextContent);
+    alert("Chose a faire ");
+    
+    
+}
+
+function cleanText(){
+    document.getElementById("btn-text").value = "";
+    
+
     
 }
