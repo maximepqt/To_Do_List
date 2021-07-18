88005ef69bcb9a07fa074f48be027cb9b3242f52
Maxime PAQUET <maxime.paquet1245@aol.fr>
2021-07-18 19:12:19 +0200

    Add remove button and done button change color of div to green

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ function choseAFaire(){
     let inputTextContent = document.getElementById("btn-text").value;
 
     var maDiv = document.createElement('div');
-    maDiv.style.cssText ='display: flex;background-color: yellow;width:200px; justify-content:center;';
+    maDiv.style.cssText ='display: flex;background-color: yellow;width:200px; justify-content:center; margin: 20px 0';
     maDiv.innerHTML = inputTextContent;
 
     var monButtonDone = document.createElement('BUTTON');
     monButtonDone.innerText="Done";
     monButtonDone.style.cssText ='margin: 0 10px; font-weight:bold;'
     monButtonDone.onclick = function changeLocation(){
+        maDiv.style.backgroundColor = "green";
+        return false;
+        }
+	var monButtonRemove = document.createElement('BUTTON');
+    monButtonRemove.innerText = "Remove";
+    monButtonRemove.style.cssText ='margin: 0 10px; font-weight:bold;'
+    monButtonRemove.onclick = function changeLocation(){
         maDiv.remove();
         return false;
         }
-	
+
 	document.getElementById('toDoAFaire').appendChild(maDiv);
     maDiv.appendChild(monButtonDone);
+    maDiv.appendChild(monButtonRemove);
 }
 
 function cleanText(){
