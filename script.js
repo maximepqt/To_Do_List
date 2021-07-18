e6a279a1de4e0a2ec3f3de59dbe5b07dca62866e
Maxime PAQUET <maxime.paquet1245@aol.fr>
2021-07-18 19:08:41 +0200

    Add Remove Function on click

--- script.js
+++ script.js
@@ -3,9 +3,20 @@
 function choseAFaire(){
     let inputTextContent = document.getElementById("btn-text").value;
 
-    var el = document.createElement('div');
-	el.innerHTML = inputTextContent;
-	document.getElementById('toDoAFaire').appendChild(el);
+    var maDiv = document.createElement('div');
+    maDiv.style.cssText ='display: flex;background-color: yellow;width:200px; justify-content:center;';
+    maDiv.innerHTML = inputTextContent;
+
+    var monButtonDone = document.createElement('BUTTON');
+    monButtonDone.innerText="Done";
+    monButtonDone.style.cssText ='margin: 0 10px; font-weight:bold;'
+    monButtonDone.onclick = function changeLocation(){
+        maDiv.remove();
+        return false;
+        }
+	
+	document.getElementById('toDoAFaire').appendChild(maDiv);
+    maDiv.appendChild(monButtonDone);
 }
 
 function cleanText(){
